4ee10b68304977c57b6d47dfb26abd3690ad9b38
abhi1nandy2 <p.taleo17@gmail.com>
2024-03-07 11:41:25 +0000

    add molecular structure info and fragmentation functions

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,7 +1,11 @@
 import pandas as pd
-from utils.config import DATA_DIR
-from utils.mol_utils import canonicalize
+from rdkit import Chem
+from tqdm import tqdm
 
+from utils.config import DATA_DIR
+from utils.mol_utils import canonicalize, mols_from_smiles
+from data.molecule_structures import count_atoms, count_bonds, count_rings, qed, sas, logp, mr
+from data.fragmentation import break_into_fragments
 def read_and_clean_dataset(info):
     raw_path = DATA_DIR / info['name'] / 'raw'
 
@@ -31,7 +35,96 @@ def read_and_clean_dataset(info):
         dataset = dataset.sample(frac=1, random_state=42)
 
     smiles = dataset.smiles.tolist()
-    dataset.smiles = [canonicalize(smi, clear_stereo=True) for smi in smiles]
+    print("Canonicalizing Molecules...")
+    dataset.smiles = [canonicalize(smi, clear_stereo=True) for smi in tqdm(smiles)]
+    # drop all null rows
     dataset = dataset[dataset.smiles.notnull()].reset_index(drop=True)
 
+    return dataset
+
+def add_atom_counts(dataset:pd.DataFrame, mols:list, info:dict)->pd.DataFrame:
+    """
+    This function adds the count of atoms in each molecule to the dataset dataframe.
+
+    Parameters:
+    dataset (pd.DataFrame): dataset of the molecules
+    mols (list of Chem.rdchem.Mol): list of molecules in Chem.rdchem.Mol format
+    info (dict): the information dictionary of the dataset
+
+    Returns:
+    pd.DataFrame: the dataframe of molecules with the atom counts concatenated.
+    """
+    print("Counting Atoms...")
+    counts = [count_atoms(mol, info['atoms']) for mol in tqdm(mols)]
+    return pd.concat([dataset, pd.DataFrame(counts)], axis=1, sort=False)
+
+
+def add_bond_counts(dataset:pd.DataFrame, mols:list, info:dict)->pd.DataFrame:
+    """
+    This function adds the count of bonds in each molecule to the dataset dataframe.
+
+    Parameters:
+    dataset (pd.DataFrame): dataset of the molecules
+    mols (list of Chem.rdchem.Mol): list of molecules in Chem.rdchem.Mol format
+    info (dict): the information dictionary of the dataset
+
+    Returns:
+    pd.DataFrame: the dataframe of molecules with the bond counts concatenated.
+    """
+    print("Counting Bonds...")
+    counts = [count_bonds(mol, info['bonds']) for mol in tqdm(mols)]
+    return pd.concat([dataset, pd.DataFrame(counts)], axis=1, sort=False)
+
+
+def add_ring_counts(dataset:pd.DataFrame, mols:list, info:dict)->pd.DataFrame:
+    """
+    This function adds the count of rings in each molecule to the dataset dataframe.
+
+    Parameters:
+    dataset (pd.DataFrame): dataset of the molecules
+    mols (list of Chem.rdchem.Mol): list of molecules in Chem.rdchem.Mol format
+    info (dict): the information dictionary of the dataset
+
+    Returns:
+    pd.DataFrame: the dataframe of molecules with the ring counts concatenated.
+    """
+    print("Counting Rings...")
+    counts = [count_rings(mol, info['rings']) for mol in tqdm(mols)]
+    return pd.concat([dataset, pd.DataFrame(counts)], axis=1, sort=False)
+
+def add_property(dataset:pd.DataFrame, mols:list, prop_name:str)->pd.DataFrame:
+    """
+    This function adds the property of each molecule to the dataset dataframe.
+    
+    Parameters:
+    dataset (pd.DataFrame): dataset of the molecules
+    mols (list of Chem.rdchem.Mol): list of molecules in Chem.rdchem.Mol format
+    prop_name (str): the name of the property to add to the dataframe
+
+    Returns:
+    pd.DataFrame: the dataframe of molecules property names
+    """
+    fn = {"qed": qed, "SAS": sas, "logP": logp, "mr": mr}[prop_name]
+    prop = [fn(mol) for mol in mols]
+    new_data = pd.DataFrame(prop, columns=[prop_name])
+    return pd.concat([dataset, new_data], axis=1, sort=False)
+
+def add_fragments(dataset:pd.DataFrame, mols:list, smiles:list)->pd.DataFrame:
+    """
+    This function adds fragments the modlecules in the dataset dataframe.
+
+    Parameters:
+    dataset (pd.DataFrame): dataset of the molecules
+    mols (list of Chem.rdchem.Mol): list of molecules in Chem.rdchem.Mol format
+    smiles (list of str): list of smiles strings
+
+    Returns:
+    dataset: the dataframe of molecules with fragments
+    """
+    results = [break_into_fragments(m, s) for m, s in zip(mols, smiles)]
+    smiles, fragments, lengths = zip(*results)
+    dataset["smiles"] = smiles
+    dataset["fragments"] = fragments
+    dataset["n_fragments"] = lengths
+    
     return dataset
