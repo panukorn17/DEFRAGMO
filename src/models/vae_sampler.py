15cdafa0063a513ac5dd9e0b8e4cddc654733996
panukorn17 <p.taleo17@gmail.com>
2024-07-04 17:43:49 +0100

    add docstring to methods and include a method to get the mean and std of training points.

--- src/models/vae_sampler.py
+++ src/models/vae_sampler.py
@@ -4,15 +4,47 @@ from torch.autograd import Variable
 import numpy as np
 import time
 import itertools
+import pickle
+import os
 
 from rdkit import Chem
 from rdkit.Chem import MolToSmiles, MolFromSmiles
-from utils.mol_utils import mol_to_smiles, mols_to_smiles, mols_from_smiles, mol_from_smiles
+from utils.mol_utils import mol_to_smiles, mols_from_smiles
+from utils.config import RUNS_DIR
 from data.fragmentation import replace_last, get_size
-def remove_consecutive(fragments):
+from data.fragment_embeddings import Vocabulary
+from data.dataset import MoleculeFragmentsDataset
+
+
+def remove_consecutive(fragments: list[str])->list[str]:
+    """
+    This function removes consecutive duplicate fragments from the list of fragments.
+
+    Parameters:
+    fragments (list): The list of fragments.
+
+    Returns:
+    list: The list of fragments with consecutive duplicates removed.
+
+    Example: ['*C(C)(C)C', '*C(=O)Cc1coc2ccc(*)cc12', '*C(=O)Cc1coc2ccc(*)cc12', '*N*', '*N*', 'c1(*)ccccc1F', , '<EOS>']
+    Output: ['*C(C)(C)C', '*C(=O)Cc1coc2ccc(*)cc12', '*N*', 'c1(*)ccccc1F', '<EOS>']
+    """
     return [i for i, _ in itertools.groupby(fragments)]
 
-def reconstruct(frags):
+def reconstruct(frags: list[str])->tuple[Chem.Mol, list[str]]:
+    """
+    This function reconstructs the molecule from the list of fragments.
+
+    Parameters:
+    frags (list): The list of fragments.
+
+    Returns:
+    tuple: The reconstructed molecule and the list of fragments.
+
+    Example: ['*C(C)(C)C', '*C(=O)Cc1coc2ccc(*)cc12', '*N*', 'c1(*)ccccc1F']
+    Output: (<rdkit.Chem.rdchem.Mol object at 0x000001A0C1E0EF80>, ['*C(C)(C)C', '*C(=O)Cc1coc2ccc(*)cc12', '*N*', 'c1(*)ccccc1F'])
+    Note that the string representation of the molecule object is CC(C)(C)c1ccc2occ(CC(=O)Nc3ccccc3F)c2c1
+    """
     try:
         frags_test = frags.copy()
         frag_2_re = frags_test[-1]
@@ -27,7 +59,18 @@ def reconstruct(frags):
         #print("Reconstruction failed")
         return None, None
 
-def generate_molecules(samples, vocab, check_eos=False):
+def generate_molecules(samples:np.ndarray, vocab:Vocabulary, check_eos:bool=False):
+    """
+    This function generates the molecules from the samples from the latent space.
+    
+    Parameters:
+    samples (np.ndarray): The vocabulary integers sampled from the latent space.
+    vocab (Vocabulary): The vocabulary object.
+    check_eos (bool): Whether to check the end of sequence token.
+
+    Returns:
+    Depending on the check_eos parameter, it returns a list of tuples or a list of booleans.
+    """
     result = []
     num_samples = samples.shape[0]
     if check_eos:
@@ -105,9 +148,70 @@ class Sampler:
         self.model = model
         self.std = std
     
-    def sample(self, num_samples):
+    def get_train_mean_std(self, dataset:MoleculeFragmentsDataset, num_training_points:int=None)->tuple[torch.Tensor, torch.Tensor]:
         """
-        Sample from the VAE model.
+        This method encodes the training data points and returns the mean and std of the latent space.
+
+        Parameters:
+        dataset (MoleculeFragmentsDataset): The dataset object.
+        loader (torch.utils.data.DataLoader): The data loader.
+        num_training_points (int): The number of training points to encode. If left empty, it encodes all the training points.
+
+        Returns:
+        torch.Tensor [latent_size]: The mean of the latent space.
+        """
+        print("Getting the mean and std of training set...")
+        config_dir = RUNS_DIR / self.config.get('run_name') / 'config'
+        # check if the mean and std files exist
+        if os.path.exists(config_dir / 'mean.pkl') and os.path.exists(config_dir / 'std.pkl'):
+            with open(config_dir / 'mean.pkl', 'rb') as f:
+                mean = pickle.load(f)
+            with open(config_dir / 'std.pkl', 'rb') as f:
+                std = pickle.load(f)
+            return mean, std
+        self.model = self.model.cpu()
+        loader = dataset.get_loader()
+        self.model.eval()
+        for idx, (src, tgt, lengths, data_index) in enumerate(loader):
+            src, tgt = Variable(src), Variable(tgt)
+            if self.config.get('use_gpu'):
+                src, tgt = src.cuda(), tgt.cuda()
+
+            if self.config.get('pred_logp') or self.config.get('pred_sas'):
+                output, mu, sigma, z, logp, sas = self.model(src, lengths)
+            else:
+                output, mu, sigma, z = self.model(src, lengths)
+            if idx == 0:
+                z_stack = z.detach().cpu()
+                mu_stack = mu.detach().cpu()
+            else:
+                z_stack = torch.vstack((z_stack, z.detach().cpu()))
+                mu_stack = torch.vstack((mu_stack, mu.detach().cpu()))
+            if idx%500 == 0:
+                print(f"Encoded {len(z_stack)} points")
+                torch.cuda.empty_cache()
+            if num_training_points is not None:
+                if len(z_stack) >= num_training_points:
+                    z_stack = z_stack[:num_training_points]
+                    break
+        mean = torch.mean(z_stack, axis=0)
+        std = torch.std(z_stack, axis=0)
+        #save the mean as a pickle file
+        with open(config_dir / 'mean.pkl', 'wb') as f:
+            pickle.dump(mean, f)
+        with open(config_dir / 'std.pkl', 'wb') as f:
+            pickle.dump(std, f)
+        return mean, std
+    
+    def sample(self, num_samples:int, train_mean:torch.Tensor, train_std:torch.Tensor)->list[str]:
+        """
+        This method sample from the latent space.
+
+        Parameters:
+        num_samples (int): The number of samples to generate.
+
+        Returns:
+        list: The list of generated molecules.
         """
         self.model = self.model.cpu()
         self.model.eval()
@@ -122,19 +226,23 @@ class Sampler:
         count = 0
         total_time = 0
         batch_size = 100
-        sample_std = self.std
+        c = self.std
         samples, sampled = [], 0
 
         max_length = self.config.get('max_len')
         temperature = self.config.get('temperature')
         sample_method = self.config.get('sampler_method')
 
+        # expand the mean and std to the batch size
+        mean = train_mean.expand(batch_size, -1)
+        std = c * train_std.expand(batch_size, -1)
+
         with torch.no_grad():
             while len(samples) < num_samples:
                 start = time.time()
 
                 # sample vector from latent space
-                z = torch.normal(0, sample_std, size=(batch_size, self.model.latent_size)).cpu()
+                z = torch.normal(mean, std).cpu()
                 #z = self.model.encoder.sample_normal(batch_size)
 
                 generated = self.sample_from_latent(z, hidden_layers, hidden_size, batch_size, max_length, temperature, sample_method, vocab)
@@ -166,7 +274,24 @@ class Sampler:
 
         return samples
     
-    def sample_from_latent(self, z, hidden_layers, hidden_size, batch_size, max_length, temperature, sample_method, vocab):
+    def sample_from_latent(self, z:torch.Tensor, hidden_layers:int, hidden_size:int, batch_size:int, max_length:int, temperature:float, sample_method:str, vocab:Vocabulary)->torch.Tensor:
+        """
+        This method samples from the latent space, calculates the probabilities for the next token for each sequence in the batch and ensures 
+        that the generated sequences produce a valid molecule. The method returns the generated sequences.
+
+        Parameters:
+        z (torch.Tensor): The tensor of latent space vectors.
+        hidden_layers (int): The number of hidden layers.
+        hidden_size (int): The hidden size.
+        batch_size (int): The batch size.
+        max_length (int): The maximum length of the sequence.
+        temperature (float): The temperature for sampling.
+        sample_method (str): The sampling method either 'greedy', 'sample_first' or 'sample_all'.
+        vocab (Vocabulary): The vocabulary object.
+
+        Returns:
+        torch.Tensor [batch_size, max_length]: The generated sequences.
+        """
         # get the initial state
         state = self.model.latent2hidden(z)
         state = state.view(hidden_layers, batch_size, hidden_size)
@@ -279,7 +404,19 @@ class Sampler:
             step += 1
         return generated
 
-    def update(self, save_to, sample, running_seqs, step):
+    def update(self, save_to:torch.Tensor, sample:torch.Tensor, running_seqs:torch.Tensor, step:int)->torch.Tensor:
+        """
+        This method updates the generated tensor with the new samples.
+
+        Parameters:
+        save_to (torch.Tensor): The tensor to save the samples to.
+        sample (torch.Tensor): The new samples.
+        running_seqs (torch.Tensor): The indices of the running sequences.
+        step (int): The step in the sequence.
+
+        Returns:
+        torch.Tensor [batch_size, max_length]: The updated tensor.
+        """
         # select only still running
         running_latest = save_to[running_seqs]
         # update token at step position
