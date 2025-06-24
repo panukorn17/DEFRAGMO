962a234d4a9e5c986a7acf6e7c16278d707ad09d
Panukorn Taleongpong <p.taleo17@gmail.com>
2025-06-24 20:30:33 +0000

    feat: add test_add_molecular_properties test

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,4 +1,6 @@
-from src.data.preprocess import pre_process_zinc, canonicalize_and_drop
+from src.data.preprocess import pre_process_zinc, canonicalize_and_drop, add_atom_counts, add_bond_counts, add_ring_counts, add_property 
+from src.utils.config import get_data_info
+from src.utils.mol_utils import mols_from_smiles
 from io import StringIO
 import pandas as pd
 
@@ -76,6 +78,145 @@ def test_read_and_clean_zinc_dataset():
     dataset = pre_process_zinc(dataset)
     dataset = canonicalize_and_drop(dataset)
     dataset_expected = pd.read_json(StringIO(expected_output_json), orient="records")
+    pd.testing.assert_frame_equal(dataset, dataset_expected)
+
+def test_add_molecular_properties():
+    input_json_str = """
+        [
+            {
+                "smiles":"CC(C)(C)c1ccc2occ(CC(=O)Nc3ccccc3F)c2c1",
+                "logP":5.0506,
+                "qed":0.7020122328,
+                "SAS":2.0840945721
+            },
+            {
+                "smiles":"CC1CC(C)CC(Nc2cncc(-c3nncn3C)c2)C1",
+                "logP":3.1137,
+                "qed":0.9289754881,
+                "SAS":3.4320038193
+            },
+            {
+                "smiles":"N#Cc1ccc(-c2ccc(OC(C(=O)N3CCCC3)c3ccccc3)cc2)cc1",
+                "logP":4.96778,
+                "qed":0.5996817382,
+                "SAS":2.4706326078
+            },
+            {
+                "smiles":"CCOC(=O)C1CCCN(C(=O)c2nc(-c3ccc(C)cc3)n3c2CCCCC3)C1",
+                "logP":4.00022,
+                "qed":0.690944353,
+                "SAS":2.8227533112
+            },
+            {
+                "smiles":"N#CC1=C(SCC(=O)Nc2cccc(Cl)c2)N=C([O-])C(C#N)C12CCCCC2",
+                "logP":3.60956,
+                "qed":0.7890271546,
+                "SAS":4.0351821383
+            }
+        ]
+    """
+    expected_output_json_str = """
+        [
+            {
+                "smiles":"CC(C)(C)c1ccc2occ(CC(=O)Nc3ccccc3F)c2c1",
+                "logP":5.0506,
+                "qed":0.7020122328,
+                "SAS":2.0840945721,
+                "C":20,
+                "F":1,
+                "N":1,
+                "O":2,
+                "Other":0,
+                "SINGLE":18,
+                "DOUBLE":8,
+                "TRIPLE":0,
+                "Tri":0,
+                "Quad":0,
+                "Pent":1,
+                "Hex":2
+            },
+            {
+                "smiles":"CC1CC(C)CC(Nc2cncc(-c3nncn3C)c2)C1",
+                "logP":3.1137,
+                "qed":0.9289754881,
+                "SAS":3.4320038193,
+                "C":16,
+                "F":0,
+                "N":5,
+                "O":0,
+                "Other":0,
+                "SINGLE":18,
+                "DOUBLE":5,
+                "TRIPLE":0,
+                "Tri":0,
+                "Quad":0,
+                "Pent":1,
+                "Hex":2
+            },
+            {
+                "smiles":"N#Cc1ccc(-c2ccc(OC(C(=O)N3CCCC3)c3ccccc3)cc2)cc1",
+                "logP":4.96778,
+                "qed":0.5996817382,
+                "SAS":2.4706326078,
+                "C":25,
+                "F":0,
+                "N":2,
+                "O":2,
+                "Other":0,
+                "SINGLE":21,
+                "DOUBLE":10,
+                "TRIPLE":1,
+                "Tri":0,
+                "Quad":0,
+                "Pent":1,
+                "Hex":3
+            },
+            {
+                "smiles":"CCOC(=O)C1CCCN(C(=O)c2nc(-c3ccc(C)cc3)n3c2CCCCC3)C1",
+                "logP":4.00022,
+                "qed":0.690944353,
+                "SAS":2.8227533112,
+                "C":24,
+                "F":0,
+                "N":3,
+                "O":3,
+                "Other":0,
+                "SINGLE":26,
+                "DOUBLE":7,
+                "TRIPLE":0,
+                "Tri":0,
+                "Quad":0,
+                "Pent":1,
+                "Hex":2
+            },
+            {
+                "smiles":"N#CC1=C(SCC(=O)Nc2cccc(Cl)c2)N=C([O-])C(C#N)C12CCCCC2",
+                "logP":3.60956,
+                "qed":0.7890271546,
+                "SAS":4.0351821383,
+                "C":20,
+                "F":0,
+                "N":4,
+                "O":2,
+                "Other":0,
+                "SINGLE":22,
+                "DOUBLE":6,
+                "TRIPLE":2,
+                "Tri":0,
+                "Quad":0,
+                "Pent":0,
+                "Hex":3
+            }
+        ]
+    """ 
+    dataset = pd.read_json(StringIO(input_json_str), orient = "records")
+    smiles = dataset.smiles.tolist()
+    mols = mols_from_smiles(smiles)
+    data_info = get_data_info("ZINC")
+    dataset = add_atom_counts(dataset, mols, data_info)
+    dataset = add_bond_counts(dataset, mols, data_info)
+    dataset = add_ring_counts(dataset, mols, data_info)
+    dataset_expected = pd.read_json(StringIO(expected_output_json_str), orient = "records")
     pd.testing.assert_frame_equal(dataset, dataset_expected)
 
 def main():
