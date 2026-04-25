0f55d661ac5f38b999d3352578cf1571cdee9aee
Panukorn Taleongpong <p.taleo17@gmail.com>
2026-04-25 17:29:45 +0000

    update test fragmentation

--- tests/test_fragmentation.py
+++ tests/test_fragmentation.py
@@ -3,11 +3,11 @@ from src.utils.mol_utils import mol_from_smiles
 
 def test_fragmentation_defragmo():
     smi = "CC(C)(C)c1ccc2occ(CC(=O)Nc3ccccc3F)c2c1"
-    mol = mol_from_smiles(smi)
-    assert break_into_fragments_defragmo(mol,smi) == ('CC(C)(C)c1ccc2occ(CC(=O)Nc3ccccc3F)c2c1', '*C(C)(C)C *C(=O)Cc1coc2ccc(*)cc12 *N* c1(*)ccccc1F', 4)
+    assert break_into_fragments_defragmo(smi, min_length=0) == ('CC(C)(C)c1ccc2occ(CC(=O)Nc3ccccc3F)c2c1', '*C(C)(C)C *C(=O)Cc1coc2ccc(*)cc12 *N* c1(*)ccccc1F', 4)
     
 
 if __name__=="__main__":
     test_fragmentation_defragmo()
+    print("all tests passed!")
 
    
